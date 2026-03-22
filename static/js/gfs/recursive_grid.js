5eeeed11583833c9a0dd44809bdc09944932e4a4
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2026-03-21 20:15:12 -0700

    Delete static directory

--- static/js/gfs/recursive_grid.js
+++
@@ -1,5 +0,0 @@
-import { getJsonSafe } from "./api.js";
-
-export async function loadRecursiveInterpretation(bbox, quality = "full") {
-  return await getJsonSafe(`/gfs/api/frame?bbox=${bbox}&quality=full`, null).then((frame) => frame?.recursiveGrid || null);
-}
